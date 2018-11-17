16199de396f8d1f176c1bd9006153dae7aff1616
ProtonWolf <ssr.vitamin@gmail.com>
2018-11-17 19:40:29 +0300

    Added lab1 client and server UDP
    Simple client sending a message
    Server waits for client message

--- ComputerNetworks/lab1/client.c
+++ ComputerNetworks/lab1/client.c
@@ -6,8 +6,43 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
+#define ip_addr "127.0.0.1"
+#define message_len 512
+#define socket_port 31337
+
 int main(void)
 {
     printf("Hello Client!\n");
+
+    struct sockaddr_in server_sockaddr;
+    int sock_desc;
+    char buf[message_len];
+
+    if ((sock_desc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    {
+        perror("Error socket()\n");
+        exit(1);
+    }
+
+    memset((char*)&server_sockaddr, 0, sizeof(server_sockaddr));
+    server_sockaddr.sin_family = AF_INET;
+    server_sockaddr.sin_port = htons(socket_port);
+
+    if (!inet_aton(ip_addr, &server_sockaddr.sin_addr))
+    {
+        printf("inet_aton() error!\n");
+        exit(1);
+    }
+
+    printf("Please, enter a message: \n");
+    fgets(buf, message_len, stdin);
+    if (sendto(sock_desc, buf, message_len, 0, &server_sockaddr, sizeof(server_sockaddr)) == -1)
+    {
+        perror("Error sendto()\n");
+        exit(1);
+    }
+
+    close(sock_desc);
+    
     return 0;
 }
--- ComputerNetworks/lab1/server.c
+++ ComputerNetworks/lab1/server.c
@@ -6,8 +6,47 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
+#define ip_addr "127.0.0.1"
+#define message_len 512
+#define socket_port 31337
+
 int main(void)
 {
     printf("Hello Server!\n");
+
+    struct sockaddr_in server_sockaddr, client_sockaddr;
+    int sock_desc;
+    int cslen = sizeof(client_sockaddr);
+    char buf[message_len];
+
+    if ((sock_desc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    {
+        perror("Error socket()\n");
+        exit(1);
+    }
+
+    memset((char*)&server_sockaddr, 0, sizeof(server_sockaddr));
+    server_sockaddr.sin_family = AF_INET;
+    server_sockaddr.sin_port = htons(socket_port);
+    server_sockaddr.sin_addr.s_addr = htonl(INADDR_ANY); //???
+    
+    if (bind(sock_desc, &server_sockaddr, sizeof(server_sockaddr)) == -1)
+    {
+        perror("Error bind()\n");
+        exit(1);
+    }
+
+    while (1)
+    {
+        if (recvfrom(sock_desc, buf, message_len, 0, &client_sockaddr, &cslen) == -1)
+        {
+            perror("Error recvfrom()\n");
+            exit(1);
+        }
+        printf("Received message from: %s:%d\n '%s'", inet_ntoa(client_sockaddr.sin_addr), ntohs(client_sockaddr.sin_port), buf);
+    }
+
+    close(sock_desc);
+
     return 0;
 }
