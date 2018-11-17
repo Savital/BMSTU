4831481565f8457351b700bcd984c3aef8d65faf
ProtonWolf <ssr.vitamin@gmail.com>
2018-11-17 17:43:22 +0300

    Initial Computer networks labs

---
+++ ComputerNetworks/lab1/client.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+int main(void)
+{
+    printf("Hello Client!\n");
+    return 0;
+}
