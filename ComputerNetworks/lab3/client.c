e3bd256aca07e7f42f24b03da2e14cb6433b4d8c
ProtonWolf <ssr.vitamin@gmail.com>
2018-11-18 11:27:27 +0300

    init

---
+++ ComputerNetworks/lab3/client.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+int main(void)
+{
+
+    
+    return 0;
+}
