3b43ce5465fda86e62c9e074210d0ec9f04a9355
Savostyanov Vitaliy <ssr.vitamin@gmail.com>
2018-12-11 01:18:26 +0300

    System Programming init

---
+++ SystemProgramming/main.py
@@ -0,0 +1,22 @@
+print("OS")
+
+import sys
+from PyQt5 import uic, QtWidgets, QtGui
+from PyQt5.QtCore import QObject, pyqtSlot, Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QListWidgetItem, QTableWidgetItem
+
+import numpy
+from numpy.linalg.linalg import LinAlgError
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.UI = uic.loadUi("MainForm.ui", self)
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+main()
