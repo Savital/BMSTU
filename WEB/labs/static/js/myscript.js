f73db6620762cab5bcb2ff96961058db5debe7c7
Savostyanov Vitaliy <ssr.vitamin@gmail.com>
2018-12-15 23:44:29 +0300

    myscript for http requests added

--- WEB/labs/static/js/myscript.js
+++ WEB/labs/static/js/myscript.js
@@ -1,14 +1,44 @@
+// using jQuery
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+
 function send()
 {
     alert("Hello");
-    var req_url = $("input[name='url']").value;
-	var req_method = $("select[name='method']").value;
+    var req_url = $("input[name='url']").val();
+	var req_method = $("select[name='method']").val();
 	$.ajax
 	    ({
 		    url: "send/",
 		    method: "POST",
 		    data:
-		    { csrfmiddlewaretoken: "{{ csrf_token }}",
+		    {
 			    url: req_url,
 			    method: req_method
 		    },
